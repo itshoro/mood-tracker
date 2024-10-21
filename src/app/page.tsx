b3f59e2a84f37be8b9034cb58a2a11af8904b824
Melanie Six <melanie-six@hotmail.com>
2024-10-21 17:11:38 +0200

    feat: add mood and event selection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,81 @@
+"use client";
+
+import { availableMoods, MoodSelection } from "./components/moodSelection";
+import { MoodThinking } from "./components/icons/mood-thinking";
+import { useState } from "react";
+import { EventSelection } from "./components/eventSelection";
+import { availableEvents } from "./components/eventSelection";
+
 export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+    const [moodSelected, setMoodSelected] = useState(false);
+    return (
+        <main className="p-4">
+            <div className="flex flex-col">
+                <div>
+                    <h1 className="text-2xl font-bold text-slate-800">
+                        Welcome,
+                    </h1>
+                    <h1 className="text-3xl font-bold text-slate-800">
+                        Melanie
+                    </h1>
+                </div>
+
+                <p className="mt-8 text-sm font-semibold text-slate-600">
+                    Saturday, October 19
+                </p>
+
+                <section>
+                    <h2 className="mb-6 text-2xl font-medium text-slate-800">
+                        How are you feeling today?
+                    </h2>
+
+                    <div className="rounded-lg p-4 font-semibold">
+                        {!moodSelected ? (
+                            <div className="flex flex-col items-center text-slate-800">
+                                <MoodThinking></MoodThinking>
+                                <p className="mt-4">
+                                    You haven't selected a mood yet.
+                                </p>
+                            </div>
+                        ) : null}
+
+                        <div className="mt-12 grid grid-cols-2 gap-6">
+                            {availableMoods.map((mood) => (
+                                <MoodSelection key={mood} mood={mood} />
+                            ))}
+                        </div>
+                    </div>
+                </section>
+
+                <section>
+                    <h2 className="mb-6 mt-12 text-2xl font-medium text-slate-800">
+                        Today's events
+                    </h2>
+                    <div className="flex flex-wrap gap-3">
+                        {availableEvents.map((event) => (
+                            <EventSelection
+                                key={event}
+                                event={event}
+                            ></EventSelection>
+                        ))}
+                    </div>
+                </section>
+
+                <section>
+                    <h2 className="mb-6 mt-12 text-2xl font-medium text-slate-800">
+                        My diary
+                    </h2>
+                    <div>
+                        <label htmlFor="notice"></label>
+                        <textarea
+                            id="notice"
+                            name="notice"
+                            className="h-48 w-full rounded-lg border border-gray-200 p-4 font-semibold text-slate-800 outline-none focus:border-transparent focus:ring-2 focus:ring-black focus:ring-offset-4"
+                            placeholder="Lorem Ipsum..."
+                        ></textarea>
+                    </div>
+                </section>
+            </div>
+        </main>
+    );
 }
