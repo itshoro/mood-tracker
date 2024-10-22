dfe5d750f70a64df33b455da15b27d2460bdafbf
Melanie Six <melanie-six@hotmail.com>
2024-10-22 10:12:24 +0200

    feat: add save button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,58 +23,68 @@ export default function Home() {
                 <p className="mt-8 text-sm font-semibold text-slate-600">
                     Saturday, October 19
                 </p>
+                <form>
+                    <section>
+                        <h2 className="mb-6 text-2xl font-medium text-slate-800">
+                            How are you feeling today?
+                        </h2>
 
-                <section>
-                    <h2 className="mb-6 text-2xl font-medium text-slate-800">
-                        How are you feeling today?
-                    </h2>
+                        <div className="rounded-lg p-4 font-semibold">
+                            {!moodSelected ? (
+                                <div className="flex flex-col items-center text-slate-800">
+                                    <MoodThinking></MoodThinking>
+                                    <p className="mt-4">
+                                        You haven't selected a mood yet.
+                                    </p>
+                                </div>
+                            ) : null}
 
-                    <div className="rounded-lg p-4 font-semibold">
-                        {!moodSelected ? (
-                            <div className="flex flex-col items-center text-slate-800">
-                                <MoodThinking></MoodThinking>
-                                <p className="mt-4">
-                                    You haven't selected a mood yet.
-                                </p>
+                            <div className="mt-12 grid grid-cols-2 gap-6">
+                                {availableMoods.map((mood) => (
+                                    <MoodSelection key={mood} mood={mood} />
+                                ))}
                             </div>
-                        ) : null}
+                        </div>
+                    </section>
 
-                        <div className="mt-12 grid grid-cols-2 gap-6">
-                            {availableMoods.map((mood) => (
-                                <MoodSelection key={mood} mood={mood} />
+                    <section>
+                        <h2 className="mb-6 mt-12 text-2xl font-medium text-slate-800">
+                            Today's events
+                        </h2>
+                        <div className="flex flex-wrap gap-3">
+                            {availableEvents.map((event) => (
+                                <EventSelection
+                                    key={event}
+                                    event={event}
+                                ></EventSelection>
                             ))}
                         </div>
-                    </div>
-                </section>
+                    </section>
 
-                <section>
-                    <h2 className="mb-6 mt-12 text-2xl font-medium text-slate-800">
-                        Today's events
-                    </h2>
-                    <div className="flex flex-wrap gap-3">
-                        {availableEvents.map((event) => (
-                            <EventSelection
-                                key={event}
-                                event={event}
-                            ></EventSelection>
-                        ))}
-                    </div>
-                </section>
+                    <section>
+                        <h2 className="mb-6 mt-12 text-2xl font-medium text-slate-800">
+                            My diary
+                        </h2>
+                        <div>
+                            <label htmlFor="notice"></label>
+                            <textarea
+                                id="notice"
+                                name="notice"
+                                className="h-48 w-full rounded-lg border border-gray-200 p-4 font-semibold text-slate-800 outline-none focus:border-transparent focus:ring-2 focus:ring-black focus:ring-offset-4"
+                                placeholder="Lorem Ipsum..."
+                            ></textarea>
+                        </div>
+                    </section>
 
-                <section>
-                    <h2 className="mb-6 mt-12 text-2xl font-medium text-slate-800">
-                        My diary
-                    </h2>
-                    <div>
-                        <label htmlFor="notice"></label>
-                        <textarea
-                            id="notice"
-                            name="notice"
-                            className="h-48 w-full rounded-lg border border-gray-200 p-4 font-semibold text-slate-800 outline-none focus:border-transparent focus:ring-2 focus:ring-black focus:ring-offset-4"
-                            placeholder="Lorem Ipsum..."
-                        ></textarea>
+                    <div className="sticky bottom-2 mt-8 flex justify-center">
+                        <button
+                            className="text-skate-800 bg w-full rounded-lg bg-amber-300 py-4 text-lg font-semibold text-slate-900 focus:ring-2 focus:ring-amber-300 focus:ring-offset-4"
+                            type="submit"
+                        >
+                            Save
+                        </button>
                     </div>
-                </section>
+                </form>
             </div>
         </main>
     );
