95786661b412c04715078e4466dc4ebecbcd5531
Melanie Six <melanie-six@hotmail.com>
2024-10-22 10:53:54 +0200

    feat: change sm breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {},
-  plugins: [],
+    content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        screens: {
+            sm: "350px",
+        },
+    },
+    plugins: [],
 };
 export default config;
