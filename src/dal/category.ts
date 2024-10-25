845218355e44ff995f5fda4835a71a36c1da46a7
Tim Nelke <tim.nelke@outlook.com>
2024-10-25 19:20:59 +0200

    feat: add dal for mood-ratings, tags and tag categories

--- src/dal/category.ts
+++ src/dal/category.ts
@@ -4,18 +4,18 @@ import { db } from "@/drizzle";
 import * as schema from "@/drizzle/schema";
 import { eq } from "drizzle-orm";
 
-export async function getOne(id: typeof schema.tags.$inferSelect.id) {
+export async function getOne(id: typeof schema.tagCategories.$inferSelect.id) {
     const result = await db
         .select()
-        .from(schema.tags)
-        .where(eq(schema.tags.id, id));
+        .from(schema.tagCategories)
+        .where(eq(schema.tagCategories.id, id));
 
     const tag = result.pop();
     return tag;
 }
 
 export async function getAll() {
-    const result = await db.select().from(schema.tags);
+    const result = await db.select().from(schema.tagCategories);
 
     return result;
 }
